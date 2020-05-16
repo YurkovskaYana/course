bdfb9939479f92856a76ec263ed908a3961cb487
YurkovskaYana <yurkovska@stud.onu.edu.ua>
2020-05-17 02:40:42 +0300

    .

--- app/src/main/java/com/example/core/MainActivity.java
+++ app/src/main/java/com/example/core/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.core;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.view.MotionEvent;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean dispatchGenericMotionEvent(MotionEvent ev) {
         return super.dispatchGenericMotionEvent(ev);
+    }
+
+    @Override
+    public boolean bindIsolatedService(Intent service, int flags, String instanceName, Executor executor, ServiceConnection conn) {
+        return super.bindIsolatedService(service, flags, instanceName, executor, conn);
     }
 
     public static void gtr(){}
